5e42f7b473caed622f1c40c4ba3a3341098c06f5
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-05-14 14:25:43 +0200

    ninja level 1: exercise 2

---
+++ Exercises - Ninja Level 1/2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("value of x: ", x)
+	fmt.Println("value of y: ", y)
+	fmt.Println("value of z: ", z)
+}
