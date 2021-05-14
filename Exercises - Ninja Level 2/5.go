37b7e2b36926a124e99c764b41d6e9e11bb8194d
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-05-14 22:28:50 +0200

    ninja level 2: exercise 5

---
+++ Exercises - Ninja Level 2/5.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := `Hello Nubbie, hope i see this later in life and smile like i used to smile whenever i see you.
+	
+	"baka"`
+	fmt.Println(s)
+}
