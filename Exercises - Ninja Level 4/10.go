5a951f676ea74ac25affc600b3e725cdafeb3a49
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-05-16 01:49:33 +0200

    ninja level 4: exercise 10

---
+++ Exercises - Ninja Level 4/10.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string][]string{
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	x["Musty"] = []string{"Nubbie"}
+
+	delete(x, "no_dr")
+	for k, v := range x {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+}
